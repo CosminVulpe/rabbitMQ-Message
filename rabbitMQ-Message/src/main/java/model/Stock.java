b71c68c1661be71303081f6999d8e61f5411816a
Vulpe Cosmin <cosmin98vulpe@gmail.com>
2022-10-09 10:55:30 +0300

    Add stock class model

---
+++ rabbitMQ-Message/src/main/java/model/Stock.java
@@ -0,0 +1,41 @@
+package model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Stock {
+
+    @XmlElement(name = "product_id")
+    private Integer id;
+    @XmlElement(name = "quantity")
+    private Long quantity;
+
+    public Stock() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setStock(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "id=" + id +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
